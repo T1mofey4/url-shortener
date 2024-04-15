895c405ec4ee2ac0551bac7cd7d62f2506aad138
t1mofey4 <timofeyvalerievich@gmail.com>
2024-04-15 17:58:08 +0300

    :construction: work in progress

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/T1mofey4/url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -19,4 +27,14 @@ func main() {
 	// TODO: init router: chi, "chi, render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	}
 }
