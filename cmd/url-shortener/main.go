dde4317eed421a145568cf10bb0531ac6e418b6d
t1mofey4 <timofeyvalerievich@gmail.com>
2024-05-12 01:15:39 +0300

    added

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
+	"github.com/T1mofey4/url-shortener/internal/http-server/handlesr/url/save"
+	mwLogger "github.com/T1mofey4/url-shortener/internal/http-server/middleware/logger"
+
 	"github.com/T1mofey4/url-shortener/internal/config"
+	"github.com/T1mofey4/url-shortener/internal/lib/logger/handlers/slogpretty"
 	"github.com/T1mofey4/url-shortener/internal/lib/logger/sl"
 	"github.com/T1mofey4/url-shortener/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -29,6 +36,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/url", save.New(log, storage))
+
+	log.Info("starting service", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.Idle_timeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 	// TODO: run server
 }
 
@@ -37,7 +71,8 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
+		// log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
@@ -47,3 +82,17 @@ func setupLogger(env string) *slog.Logger {
 
 	return log
 }
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
+
+// CONFIG_PATH=./config/local.yaml go run ./cmd/url-shortener/main.go
